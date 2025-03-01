3795ee4fe65f87e676eff71c2497d00115db348f
nicolasbicalho <nicolasbicalho@id.uff.br>
2025-03-01 13:20:58 -0300

    fix: error handler next

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,15 +1,17 @@
 import { Request, Response, NextFunction } from "express";
 import AppError from "../utils/AppError";
 
-const errorHandler = (err: Error | AppError, _req: Request, res: Response, _next: NextFunction) => {
+const errorHandler = (err: Error | AppError, _req: Request, res: Response, next: NextFunction) => {
 	if (err instanceof AppError) {
-    return res.status(err.statusCode).json({ message: err.message });
+    res.status(err.statusCode).json({ message: err.message });
+    return next();
   }
   console.error(err.stack);
 
   res.status(500).json({
     message: 'Internal Server Error',
   });
+  return next();
 };
 
 export default errorHandler;
